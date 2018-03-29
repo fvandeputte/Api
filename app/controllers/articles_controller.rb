8843d3d7a971e41d859785ee1f948369ce4e3ee1
fvandeputte <fvandeputtem@gmail.com>
2018-03-29 00:14:50 -0300

    agregado post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   # POST /articles
   def create
-    @article = Article.create(article_params)
+    @article = Article.create(title: 'Noticia', subtitle: 'Subtitulo', body: 'Cuerpo')
     @articles = Article.all
     render json: @articles, :except => [:updated_at]
     #if @article.save
