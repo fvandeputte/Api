6544e8504e14220e35a53743a37fe4136ebe8975
fvandeputte <fvandeputtem@gmail.com>
2018-03-28 23:25:32 -0300

    agregado post

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/news' => 'articles#index'
+  post '/news' => 'articles#new'
 end
