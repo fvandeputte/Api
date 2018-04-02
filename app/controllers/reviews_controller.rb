5917e8621f6baf91d0b66247b7bb1381610ec5c1
fvandeputte <fvandeputtem@gmail.com>
2018-04-02 19:22:11 -0300

    listo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -47,9 +47,9 @@ class ReviewsController < ApplicationController
             @review = Review.where(id: params[:id_comment], article_id: params[:id_article])
             if not @review.empty?  
               @review.first.update(review_params)
-              render json: @review, status: 200, location: @review, content_type: "application/json"
+              render json: @review.first, status: 200, location: @review, content_type: "application/json"
             else
-              render json: @review, status: 404
+              render :json => {:error => "Not found"}.to_json, :status => 404, content_type: "application/json"
             end
         else
             render :json => {:error => "Should send all the fields"}.to_json, :status => 404, content_type: "application/json"
