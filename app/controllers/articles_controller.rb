c0a84b1efa6b421ba51fa9a9de8d611024d7e908
fvandeputte <fvandeputtem@gmail.com>
2018-04-02 11:40:51 -0300

    agregado antes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
     if @article.save
       render json: @article, status: :created, location: @article, content_type: "application/json"
     else
-      render json: @article.errors, status: :unprocessable_entity
+      render json: @article.errors, status: :unprocessable_entity, content_type: "application/json"
     end
   end
 
