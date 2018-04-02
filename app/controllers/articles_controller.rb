271e746a252ae2a1ad9fcb0f1a00b138a96e53bc
fvandeputte <fvandeputtem@gmail.com>
2018-04-02 12:49:08 -0300

    nueva

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     if not @article.empty?
       render json: @article.first, :except => [:updated_at]
     else
-      render :json => {:error => "not-found"}.to_json, :status => 404, content_type: "application/json"
+      render :json => {:error => "Not found"}.to_json, :status => 404, content_type: "application/json"
     end
     #@article = Article.where(id: params[:id])
     #if not @article.empty?  
