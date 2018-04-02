20fb0010ba350ea5b31d2ddee8662334e41af66c
fvandeputte <fvandeputtem@gmail.com>
2018-04-02 10:52:35 -0300

    agregado tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,38 +5,38 @@ class ArticlesController < ApplicationController
   # GET /articles
   def index
     @articles = Article.all
-    render json: json_params_for(@articles)
+    render json: json_params_for(@articles), , content_type: "application/json"
     
   end
 
   # GET /articles/1
   def show
-    render json: @article, :except => [:updated_at]
+    render json: @article, :except => [:updated_at], content_type: "application/json"
   end
 
   # POST /articles
   def create
     @article = Article.new(title: params[:title], subtitle: params[:subtitle], body: params[:body])
     if @article.save
-      render json: @article, status: :created, location: @article
+      render json: @article, status: :created, location: @article, , content_type: "application/json"
     else
-      render json: @article.errors, status: :unprocessable_entity
+      render json: @article.errors, status: :unprocessable_entity, , content_type: "application/json"
     end
   end
 
   # PATCH/PUT /articles/1
   def update
     if @article.update(article_params)
-      render json: @article
+      render json: @article, , content_type: "application/json"
     else
-      render json: @article.errors, status: :unprocessable_entity
+      render json: @article.errors, status: :unprocessable_entity, , content_type: "application/json"
     end
   end
 
   # DELETE /articles/1
   def destroy
     @article.destroy
-    render json: @article
+    render json: @article, , content_type: "application/json"
   end
 
   private
