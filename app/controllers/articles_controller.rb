99278235b0683a7d01f1206cc77c6a22b92cf5cd
fvandeputte <fvandeputtem@gmail.com>
2018-04-02 11:08:41 -0300

    con binary

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   def index
     @articles = Article.all
-    render json: json_params_for(@articles), headers['Content-type'] = "application/json"
+    render json: json_params_for(@articles)
     
   end
 
